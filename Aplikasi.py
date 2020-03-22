561c000e2e0a5719148001314e3b5befd70fac1e
Virgileanda Nur Rifqhi <62491473+virgilnr@users.noreply.github.com>
2020-03-22 14:08:58 +0700

    Update And Name Change
    Change the back-end code to QnAftr.py and Fix unnecessary syntax thus, get some fixed!

--- Aplikasi.py
+++
@@ -1,8 +0,0 @@
-nama = input('siapa namanya bro?? ')
-umur = input('umurmu berapa bro?? ')
-cita = input('kamu mau jadi apa?? ')
-hobi = input('kalau waktu luang seringnya ngapain?? ')
-Question = input('Apakah Virgi ganteng (Y/N)? ')
-Y = print('Sangat tampan')
-N = print('Benar - Benar tampan')
-print('haloo masbro..salam kenal, ' + nama + '.' + ' kamu berumur ' + umur + ' tahun.' + ' senang bertemu :)' + '.' + ' wahh..' + cita + ' masa depan nih,' +' kamu suka ' + hobi + ' kan?.'+ 'Oh iya, Aku Virgi. Btw, makasih ya udah nganggep aku ' + Question)
---
+++ QnAFtr.py
@@ -0,0 +1,8 @@
+nama = input('siapa namanya bro?? ')
+umur = input('umurmu berapa bro?? ')
+cita = input('kamu mau jadi apa?? ')
+hobi = input('kalau waktu luang seringnya ngapain?? ')
+Y = print('Sangat tampan')
+N = print('Benar - Benar tampan')
+Question = input('Apakah Virgi ganteng (Y/N)? ')
+print('haloo masbro..salam kenal, ' + nama + '.' + ' kamu berumur ' + umur + ' tahun.' + ' senang bertemu :)' + '.' + ' wahh..' + cita + ' masa depan nih,' +' kamu suka ' + hobi + ' kan?.'+ 'Oh iya, Aku Virgi. Btw, makasih ya udah nganggep aku ganteng banget')
