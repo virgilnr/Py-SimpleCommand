cd82e222ce26e88b54677a305df26291b6c1b793
Virgileanda Nur Rifqhi <62491473+virgilnr@users.noreply.github.com>
2020-03-22 09:57:49 +0700

    Start Building DebtPayApp.py

---
+++ DebtPay.py
@@ -0,0 +1,10 @@
+##DebtPayApp
+debt = 789000
+money = input('how much money do you have?')
+
+if int(money) < debt:
+  print('Sorry sir, looks like your money is not enough to completely pay off the debt. Do you still want to proceed your payment?, then th debt left will be:' , debt-int(money))
+elif int(money)==debt:
+  print('Your money is precisely enough to pay off all the debt. Proceeding to continue your payment...')
+else:
+  print('Your money is more than enough to pay off the debt completely, here is the change:' , int(money)-debt)
