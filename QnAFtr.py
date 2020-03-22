dd55498114f20b474ca21519939fe6654f09cb06
Virgileanda Nur Rifqhi <62491473+virgilnr@users.noreply.github.com>
2020-03-22 14:10:41 +0700

    Adding If & Else Function

--- QnAFtr.py
+++ QnAFtr.py
@@ -5,4 +5,8 @@ hobi = input('kalau waktu luang seringnya ngapain?? ')
 Y = print('Sangat tampan')
 N = print('Benar - Benar tampan')
 Question = input('Apakah Virgi ganteng (Y/N)? ')
+if Question==Y:
+  print('Sangat Ganteng')
+else:
+  print('Ganteng Banget!')
 print('haloo masbro..salam kenal, ' + nama + '.' + ' kamu berumur ' + umur + ' tahun.' + ' senang bertemu :)' + '.' + ' wahh..' + cita + ' masa depan nih,' +' kamu suka ' + hobi + ' kan?.'+ 'Oh iya, Aku Virgi. Btw, makasih ya udah nganggep aku ganteng banget')
